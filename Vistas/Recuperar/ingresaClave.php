cd5a3e0b318a414abaa5ee5e8967c89e22495b8c
Miguel Angel <orjuelaparra@gmail.com>
2020-07-24 20:54:42 -0500

    Diseño recuperación de contraseña

--- Vistas/Recuperar/ingresaClave.php
+++ Vistas/Recuperar/ingresaClave.php
@@ -1,19 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body onload="document.getElementById('correo').value = correo;">
-        <p>Para seguir con tu transaccion por favor no recargar esta pagina.</p>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Agendamiento/Assets/Bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="/Agendamiento/Assets/Diseño/estilos.css">
+    <link rel="stylesheet" href="/Agendamiento/Assets/Diseño/normalize.css">
+    <link rel="icon" type="image/png" href="/Agendamiento/Assets/Imagenes/Icono.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>DJANE</title>
+</head>
+
+<body onload="document.getElementById('correo').value = correo;">
+    <nav class="nav navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="/Agendamiento/Assets/Imagenes/djlogo.png" alt="D'JANE" width="100" height="40">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link navl" href="#servicio">Servicio
+                        <!--span class="sr-only">(current)</span--></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link navl" href="Vistas/Producto/index_producto.php">Productos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link navl" href="/Agendamiento/?c=cliente&a=llamar">Registrarse</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/Agendamiento/Vistas/Home/Login.php" class="btn btn-outline-primary">Iniciar sesión</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="box">
+        <h3>Para seguir con tu transacción por favor no recargar esta página.</h3>
 
         <form action="/Agendamiento/?c=valida&a=cambio" method="POST" id="Formulario">
-            <input type="password" name="psw1" id="">
-            <input type="password" name="psw2" id="">
-            <input type="hidden" name="correo" id="correo">
-            <button type="submit">Cambiar</button>
-        </form>
+            <div>
+                <input type="password" name="psw1" id="" required>
+                <label>Ingresar la nueva contraseña</label>
+            </div>
+            <div>
+                <input type="password" name="psw2" id="" required>
+                <label>Ingresar la nueva contraseña</label>
+            </div>
 
-    </body>
+
+
+            <input type="hidden" name="correo" id="correo">
+            <button type="submit" class="btn btn-block btn-primary">Cambiar</button>
+        </form>
+    </div>
+
+
+</body>
+<script src="/Agendamiento/Assets/jquery-3.5.1.min.js"></script>
+<script src="/Agendamiento/Assets/Bootstrap/js/bootstrap.js"></script>
+
 </html>
--- Vistas/Recuperar/ingresaCodigo.php
+++ Vistas/Recuperar/ingresaCodigo.php
@@ -1,21 +1,60 @@
-
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="/Agendamiento/Assets/Bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="/Agendamiento/Assets/Diseño/estilos.css">
+    <link rel="stylesheet" href="/Agendamiento/Assets/Diseño/normalize.css">
+    <link rel="icon" type="image/png" href="/Agendamiento/Assets/Imagenes/Icono.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>DJANE</title>
 </head>
+
 <body>
-    <form action="/Agendamiento/?c=valida&a=codigo" method="post">
-    <p>Escribe el codigo que enviamos a tu correo</p>
-    <input autocomplete="off" name="codigo" type="text">
-    
-   <input type="hidden" name="numero" value="<?php print ($_SESSION['idContra']);?>">
-   <input type="hidden" name="correo" value="<?php print ($_SESSION['correo']);?>">
-   <input type="submit" value="Validar" >
-    </form>
+    <nav class="nav navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="/Agendamiento/Assets/Imagenes/djlogo.png" alt="D'JANE" width="100" height="40">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link navl" href="#servicio">Servicio
+                        <!--span class="sr-only">(current)</span--></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link navl" href="Vistas/Producto/index_producto.php">Productos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link navl" href="/Agendamiento/?c=cliente&a=llamar">Registrarse</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/Agendamiento/Vistas/Home/Login.php" class="btn btn-outline-primary">Iniciar sesión</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="box">
+        <form action="/Agendamiento/?c=valida&a=codigo" method="post">
+            <h3>Escribe el código que enviamos a tu correo</h3>
+            <div>
+            <input autocomplete="off" name="codigo" type="text" required>
+                <label>Ingresar código</label>
+            </div>
+          
+
+            <input type="hidden" name="numero" value="<?php print($_SESSION['idContra']); ?>">
+            <input type="hidden" name="correo" value="<?php print($_SESSION['correo']); ?>">
+            <input type="submit" value="Validar" class="btn btn-block btn-primary">
+        </form>
+    </div>
+    <script src="/Agendamiento/Assets/jquery-3.5.1.min.js"></script>
+    <script src="/Agendamiento/Assets/Bootstrap/js/bootstrap.js"></script>
+
 </body>
+
 </html>
