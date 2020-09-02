8229fe476b6277f82ff6a3cb21f667ed0a82a4d9
Miguel Angel <orjuelaparra@gmail.com>
2020-09-02 16:53:46 -0500

    Diseño pagina no existe

---
+++ Exception/noExiste.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Agendamiento/Assets/Bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="/Agendamiento/Assets/Diseño/estilos.css">
+    <link rel="stylesheet" href="/Agendamiento/Assets/Diseño/normalize.css">
+    <link rel="icon" type="image/png" href="/Agendamiento/Assets/Imagenes/Icono.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>No existe</title>
+</head>
+
+<body>
+
+
+
+
+
+    <section class="container">
+
+
+        <div class="jumbotron">
+
+            <div class="text-center">
+                <img src="/Agendamiento/Assets/Imagenes/djlogo.png" alt="D´JANE">
+            </div>
+
+            <h1 class="display-4">ERROR 404</h1>
+            <p class="lead">Lamentamos el inconveniente, hubo un problema en nuestra página.</p>
+            <hr class="my-4">
+            <p>Comuniquese con el administrador.</p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Volver al inicio</a>
+        </div>
+
+        <div class="text-center">
+
+            <a href="#" class="p-3" onclick="facebook()"><i class="fa fa-facebook f"></i></a>
+            <a href="#" class="p-3" onclick="instagram()"><i class="fa fa-instagram i"></i></a>
+            <a href="#" class="p-3" onclick="youtube()"><i class="fa fa-youtube y" ></i></a>
+            <a href="#" class="p-3" onclick="whatsapp()"><i class="fa fa-whatsapp w"></i></a>
+
+        </div>
+
+        <p class="text-center p-4 text-secondary"> Visite nuestras redes sociales</p>
+        <hr>
+        <style>
+        .fa {
+            font-size: 51px;
+            transition: 0.5s;
+            color: black;
+        }
+
+        .f:hover{
+            font-size: 60px;
+            color: #007BFF;
+            background: #ffff;
+            border: 2px solid #007BFF;
+            padding: 8px;
+            border-radius: 5px;
+        
+        }
+
+        .i:hover{
+            font-size: 60px;
+            color: #E85030;
+            background: #ffff;
+            border: 2px solid #A24CC8;
+            padding: 8px;
+            border-radius: 5px;
+        
+        }
+
+        .w:hover{
+            font-size: 60px;
+            color: #00E676;
+            background: #ffff;
+            border: 2px solid #00E676;
+            padding: 8px;
+            border-radius: 5px;
+        
+        }
+
+        .y:hover{
+            font-size: 60px;
+            color: #CC0000;
+            background: #ffff;
+            border: 2px solid #CC0000;
+            padding: 8px;
+            border-radius: 5px;
+        
+        }
+        </style>
+    </section>
+
+</body>
+<script src="/Agendamiento/Assets/Funciones/funciones.js"></script>
+
+</html>
