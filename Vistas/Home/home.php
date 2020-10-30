3ce43facd50bd37a5c56ebac41dec12263d2d5eb
Jairmt <37247987+Jairmt@users.noreply.github.com>
2020-10-30 12:53:13 -0500

    Correccion Home Servicio...

--- Vistas/Home/home.php
+++ Vistas/Home/home.php
@@ -1,3 +1,10 @@
+<?php
+session_start();     
+$Servicio;
+        $this->Servicio = new Servicio();
+                  $resultado = $this->Servicio->listar();
+          ?>
+
 <!DOCTYPE html>
 <html lang="es">
 
